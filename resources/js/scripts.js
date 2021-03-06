601d011ce04c8d445ed6ceb2e931bb8a79942a1e
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-03-06 23:02:19 +0000

    adding animation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop: $(".js--section-features").offset().top}, 1000);
     });
 
-    /* NAvigation scroll */
+    /* Navigation scroll */
     $(function() {
         $('a[href*="#"]:not([href="#"])').click(function() {
           if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -23,6 +23,44 @@ $(document).ready(function() {
           }
         });
       });
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(
+      (direction) => {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+      },
+      {
+        offset: '50%',
+      }
+    );
+
+    $('.js--wp-2').waypoint(
+      (direction) => {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+      },
+      {
+        offset: '50%',
+      }
+    );
+
+    $('.js--wp-3').waypoint(
+      (direction) => {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+      },
+      {
+        offset: '50%',
+      }
+    );
+
+    $('.js--wp-4').waypoint(
+      (direction) => {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+      },
+      {
+        offset: '50%',
+      }
+    );
+
 
 
 });
